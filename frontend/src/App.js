d85b7c35743e25e9c18e9ff3a709bd52cc0c6bec
ha165 <lumumbaharmony@gmail.com>
2024-07-31 13:30:07 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
   <>
   <Header />
-  <Outlet />
+  <main>
+    <Outlet/>
+  </main>
+  <Footer />
   </>
   );
 }
