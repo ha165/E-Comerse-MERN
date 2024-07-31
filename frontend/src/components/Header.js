6076625f31fe8d5e3885fcae40d842d4abc0169f
ha165 <lumumbaharmony@gmail.com>
2024-07-31 14:32:55 +0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from './Logo'
-import { CiSearch } from "react-icons/ci";
+import { GrSearch } from "react-icons/gr";
 const Header = () => {
   return (
     <header className='h-16 shadow-md'>
@@ -9,10 +9,10 @@ const Header = () => {
             <Logo w={100} h={50}/>
          </div>
 
-         <div className='flex items-center w-full justify-between max-w-sm'>
-           <input type='text' placeholder='Search product here...'/>
-           <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full'>
-              <CiSearch/>
+         <div className='flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2'>
+           <input type='text' placeholder='Search product here...' className='w-full outline-none'/>
+           <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+           <GrSearch />
            </div>
          </div>
 
