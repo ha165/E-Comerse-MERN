8c876022a0a18b9d436e7d65eafe569db4fb8b5f
ha165 <lumumbaharmony@gmail.com>
2024-07-31 15:48:31 +0300

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,34 @@
-import React from 'react'
-
+import React from 'react';
+import LogoIcon from '../assest/signin.gif';
 const Login = () => {
   return (
-    <div>Login</div>
+    <section id="login">
+      <div className='mx-auto container p-4'>
+        <div className='bg-white p-2 py-5 w-full max-w-md mx-auto'>
+          <div className='w-20 h-20 mx-auto'>
+            <img src={LogoIcon} alt='login icon' />
+          </div>
+          <form>
+            <div className='grid'>
+              <label> Email:</label>
+              <div v className='bg-slate-100 p-2'>
+                <input type='email' placeholder='Enter email' className='w-full h-full outline-none bg-transparent' />
+              </div>
+            </div>
+
+            <div>
+              <label> Password:</label>
+              <div>
+                <input type='password' placeholder='Enter Password' />
+              </div>
+            </div>
+
+            <button>Login</button>
+
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
