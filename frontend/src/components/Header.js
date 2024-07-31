1172b138b50f2449cae5a23212f933115b5d2d76
ha165 <lumumbaharmony@gmail.com>
2024-07-31 14:25:21 +0300

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Logo from './Logo'
+import { CiSearch } from "react-icons/ci";
 const Header = () => {
   return (
     <header className='h-16 shadow-md'>
-      <div className="h-full container mx-auto flex items-center">
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
          <div className=''>
             <Logo w={100} h={50}/>
+         </div>
+
+         <div className='flex items-center w-full justify-between max-w-sm'>
+           <input type='text' placeholder='Search product here...'/>
+           <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full'>
+              <CiSearch/>
+           </div>
+         </div>
+
+         <div>
+          user icons and cards
          </div>
       </div>
       
