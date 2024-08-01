da3320eeb7d1aee49c54adf150276ce9f0dd9f3f
ha165 <lumumbaharmony@gmail.com>
2024-08-01 11:08:36 +0300

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import LogoIcon from '../assest/signin.gif';
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import {Link} from 'react-router-dom';
 const Login = () => {
   const [showPassword, setshowPassword] = useState(false)
   return (
@@ -23,20 +24,26 @@ const Login = () => {
                 <label> Password:</label>
                 <div className='bg-slate-100 p-2 flex'>
                   <input type={showPassword ? "text" : "password"} placeholder='Enter Password' className='w-full h-full outline-none bg-transparent' />
-                  <div className='cursor-pointer text-xl'>
+                  <div className='cursor-pointer text-xl' onClick={() => setshowPassword((preve) => !preve)}>
                     <span>
-                      <FaEye />
-                      <FaEyeSlash />
+                      {
+                        showPassword ? (
+                          <FaEyeSlash />
+                        ) : (
+                          <FaEye />
+                        )
+                      }
                     </span>
 
                   </div>
                 </div>
+                <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-red-600'>
+                  Forgot password ?
+                </Link>
               </div>
             </div>
 
-
-
-            <button>Login</button>
+            <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
 
           </form>
         </div>
