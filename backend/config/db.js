b1cbf87d23e96b0b69669fba5611128d2bd37ab3
ha165 <lumumbaharmony@gmail.com>
2024-08-02 10:31:30 +0300

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+const mongoose =  require("mongoose")
+
+async function connectDB(){
+    try {
+        mongoose.connect(process.env.MONGODB_URI)
+    } catch (error) {
+        console.log(error)
+    }
+}
