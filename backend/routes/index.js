d5a618fd016df14cd7c2af1146bd388a49e8dc05
ha165 <lumumbaharmony@gmail.com>
2024-08-02 12:06:14 +0300

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const SignUpController = require("../controller/userSignUp")
 
 const router = express.Router
+
+router.post("/signup")
+
+
+
+
+
+
 
 module.exports = router
