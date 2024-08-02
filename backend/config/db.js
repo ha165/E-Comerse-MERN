1f0b4de9e3a0f38cfd1581a9ba22c6e672f74873
ha165 <lumumbaharmony@gmail.com>
2024-08-02 11:00:54 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose =  require("mongoose")
 
 async function connectDB(){
     try {
-        mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
+        
     } catch (error) {
         console.log(error)
     }
