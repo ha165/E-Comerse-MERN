ad66edca79c6a4c30aa2df7c101eebbad6381149
ha165 <lumumbaharmony@gmail.com>
2024-08-02 11:26:26 +0300

    changes

---
+++ backend/controller/userSignUp.js
@@ -0,0 +1,11 @@
+async function SignUpController(req,res){
+    try {
+        
+    } catch (error) {
+        res.json({
+            message : error,
+            error : true,
+            success : false
+        })
+    }
+}
