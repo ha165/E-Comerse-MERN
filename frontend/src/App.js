a9c1e540de090729a90b9f9e7466b8e135d16c1f
ha165 <lumumbaharmony@gmail.com>
2024-08-16 12:54:16 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,50 @@
-import React, { useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
-import summaryApi from './common';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
-import { setUserDetails } from './store/userSlice';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import summaryApi from './common';
 import Context from './context';
 import { useDispatch } from 'react-redux';
-import 'react-toastify/dist/ReactToastify.css';
+import { setUserDetails } from './store/userSlice';
 
 function App() {
-  const dispatch = useDispatch();
-
+  const dispatch = useDispatch()
   const fetchUserDetails = async () => {
-    try {
-      const dataResponse = await fetch(summaryApi.current_user.url, {
-        method: summaryApi.current_user.method,
-        credentials: 'include',
-      });
+    const dataResponse = await fetch(summaryApi.current_user.url, {
+      method: summaryApi.current_user.method,
+      credentials: 'include'
+    })
 
-      const dataApi = await dataResponse.json();
+    const dataApi = await dataResponse.json()
 
-      if (dataApi.success) {
-        dispatch(setUserDetails(dataApi.data));
-      }
-    } catch (error) {
-      console.error("Failed to fetch user details:", error);
+    if (dataApi.success) {
+      dispatch(setUserDetails(dataApi.data))
     }
-  };
-
+  }
   useEffect(() => {
-    fetchUserDetails();
-  }, []);
-
+    /**user Details */
+    fetchUserDetails()
+  }, [])
   return (
-    <Context.Provider value={{ fetchUserDetails }}>
-      <ToastContainer position="top-center" />
-      <Header />
-      <main className="min-h-[calc(100vh-120px)] pt-16">
-        <Outlet />
-      </main>
-      <Footer />
-    </Context.Provider>
+    <>
+      <Context.Provider value={{
+        fetchUserDetails, // user detail fetch 
+      }}>
+        <ToastContainer
+          position='top-center'
+        />
+
+        <Header />
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet />
+        </main>
+        <Footer />
+      </Context.Provider>
+    </>
   );
 }
 
