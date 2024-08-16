ba2be767f63163dbd3bfd4f9a1414ed9aaf8372d
ha165 <lumumbaharmony@gmail.com>
2024-08-16 12:49:38 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store'
-import router from './routes'
+import router from './routes';
+import { Provider  } from 'react-redux';
+import { store } from './store/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}/>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
